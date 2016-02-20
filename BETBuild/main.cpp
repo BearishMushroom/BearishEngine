aa8e4dab15b40fe52fe7f410f76cfe8f7045a2f5
BearishMushroom <niklas.andersson98@outlook.com>
2016-02-20 20:47:28 +0100

    BETBuild builder for new BET texture file format.

--- BETBuild/main.cpp
+++ BETBuild/main.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "BETFile.h"
 
 int main(int argc, char** argv) {
-	printf("WOOWOWOWOWO");
+	if (argc < 3) {
+		// No input was specified.
+		printf("Usage: BETBuild input.png output.bet");
+		return -1;
+	}
+
+	string inputPath = string(argv[1]);
+	string outputPath = string(argv[2]);
+
+	i32 width, height;
+	u8* image = SOIL_load_image(inputPath.c_str(), &width, &height, 0, 0);
+
+	BETFile output;
+	output.data = image;
+	output.dataSize = width * height * 4;
+	output.width = width;
+	output.height = height;
+	output.ConvertToBET();
+
+	output.WriteToFile(outputPath);
+
+	output.ConvertToRaw();
+	SOIL_save_image("test.tga", SOIL_SAVE_TYPE_TGA, output.width, output.height, 4, output.data);
+
 	return 0;
 }
